6e328551eb7195c12299561ee3be3a7b6302dd92
luccaloopz <luccamartins2012@gmail.com>
2022-05-02 21:42:16 -0700

    users now have the choice to update an employee's role. Also, I refactored the server.js file to allow for question looping by promisifying all of the db.query methods.

--- server.js
+++ server.js
@@ -67,110 +67,162 @@ const addEmployee = [
     }
 ];
 
-function init() { // How do I make it so that when I recall init() after I finish adding to or viewing a table, the table isn't formatted weirdly on the command-line questioning? see comments below... 
+const updateEmployee = [
+    {
+        type: "list",
+        message: "Which employee's role would you like to update?",
+        choices: [],
+        name: "listOfEmployees"
+    },
+    {
+        type: "list",
+        message: "Which role would you like to assign the selected employee?",
+        choices: [],
+        name: "listOfEmployeeRoles"
+    }
+];
+
+function init() {
     inquirer
     .prompt(firstQuestion)
     .then((answers) => {
         if (answers.choices === "View Departments") {
             const departments = new Departments;
-            departments.getDepartments();
-            // init(); --> this currently overrides the command-line and makes the UI look funky
+            departments.getDepartments().then(() => {
+                init(); 
+            }).catch((err) => {
+                console.log(err);
+            });
+
         } else if (answers.choices === "Add Department") {
             inquirer
             .prompt(addDep)
             .then((answers) => {
                 const departments = new Departments(answers.depName);
-                departments.addDepartment();
+                departments.addDepartment().then(() => {
+                    init(); 
+                }).catch((err) => {
+                    console.log(err);
+                });
             });
+
         } else if (answers.choices === "View Roles") {
             const roles = new Roles;
-            roles.getRoles();
-        } else if (answers.choices === "Add Role") {
-            db.query("SELECT * FROM departments", (err, data) => {
-                if (err) {
-                    console.log(err)
-                };
+            roles.getRoles().then(() => {
+                init(); 
+            }).catch((err) => {
+                console.log(err);
+            });
 
+        } else if (answers.choices === "Add Role") {
+            db.promise().query("SELECT * FROM departments").then(([data]) => {
                 for (let i = 0; i < data.length; i++) {
                     addRole[2].choices.push(data[i].dep_name);
                 };
-            });
 
-            inquirer
-            .prompt(addRole)
-            .then((answers) => {
-                const departments = new Departments(answers.roleDep);
-                departments.getSpecificDepID((err, data) => {
-                    if (err) {
-                        console.log(err)
-                    };
+                inquirer
+                .prompt(addRole)
+                .then((answers) => {
+                    const departments = new Departments(answers.roleDep);
+                    departments.getSpecificDepID().then(([data]) => {
+                        let depId = JSON.stringify(data[0].id);
 
-                    let depId = JSON.stringify(data[0].id);
-
-                    const roles = new Roles(answers.roleTitle, answers.roleSalary, depId);
-                    roles.addRole();
+                        const roles = new Roles(answers.roleTitle, answers.roleSalary, depId);
+                        roles.addRole().then(() => {
+                            init(); 
+                        });
+                    });
                 });
+            }).catch((err) => {
+                console.log(err);
             });
+
         } else if (answers.choices === "View Employees") {
             const employees = new Employees;
-            employees.getEmployees();
+            employees.getEmployees().then(() => {
+                init();
+            }).catch((err) => {
+                console.log(err);
+            });
         } else if (answers.choices === "Add Employee") {
-            db.query("SELECT title FROM roles", (err, data) => {
-                if (err) {
-                    console.log(err)
-                };
-
+            db.promise().query("SELECT title FROM roles").then(([data]) => {
                 for (let i = 0; i < data.length; i++) {
                     addEmployee[2].choices.push(data[i].title);
                 };
-            });
 
-            db.query("SELECT CONCAT(first_name, ' ', last_name) AS name FROM employees", (err, data) => {
-                if (err) {
-                    console.log(err)
-                };
-
-                for (let i = 0; i < data.length; i++) {
-                    addEmployee[3].choices.push(data[i].name);
-                };
-            });
-
-            inquirer
-            .prompt(addEmployee)
-            .then((answers) => {
-                const roles = new Roles(answers.employeeRole);
-                roles.getSpecificRoleID((err, data) => {
-                    if (err) {
-                        console.log(err)
+                db.promise().query("SELECT CONCAT(first_name, ' ', last_name) AS name FROM employees").then(([data]) => {
+                    for (let i = 0; i < data.length; i++) {
+                        addEmployee[3].choices.push(data[i].name);
                     };
 
-                    let roleId = JSON.stringify(data[0].id);
+                    inquirer
+                    .prompt(addEmployee)
+                    .then((answers) => {
+                        const roles = new Roles(answers.employeeRole);
+                        roles.getSpecificRoleID().then(([data]) => {
+                            let roleId = JSON.stringify(data[0].id);
 
-                    let nameArr = answers.employeeManager.split(' ');
+                            let nameArr = answers.employeeManager.split(' ');
 
-                    if (nameArr.length === 2) {
-                        let firstName = nameArr[0];
-                        let lastName = nameArr[1];
+                            if (nameArr.length === 2) {
+                                let firstName = nameArr[0];
+                                let lastName = nameArr[1];
 
-                        const employees1 = new Employees(firstName, lastName);
-                        employees1.getSpecificManagerID((err, data) => {
-                            if (err) {
-                                console.log(err)
+                                const employees1 = new Employees(firstName, lastName);
+                                employees1.getSpecificManagerID().then(([data]) => {
+                                    let managerId = JSON.stringify(data[0].id);
+
+                                    const employees2 = new Employees(answers.employeeFirst, answers.employeeLast, roleId, managerId);
+                                    employees2.addEmployee().then(() => {
+                                        init();
+                                    });
+                                });
+                            } else if (nameArr.length === 1) {
+                                const employees3 = new Employees(answers.employeeFirst, answers.employeeLast, roleId);
+                                employees3.addManager().then(() => {
+                                    init();
+                                });
                             };
-
-                            let managerId = JSON.stringify(data[0].id);
-
-                            const employees2 = new Employees(answers.employeeFirst, answers.employeeLast, roleId, managerId);
-                            employees2.addEmployee();
                         });
-                    } else if (nameArr.length === 1) {
-                        const employees3 = new Employees(answers.employeeFirst, answers.employeeLast, roleId);
-                        employees3.addManager();
-                    };
-                
+                    });
                 });
+            }).catch((err) => {
+                console.log(err);
             });
-        }
+             
+        } else if (answers.choices === "Update Employee Role") {
+            db.promise().query("SELECT CONCAT(first_name, ' ', last_name) AS name FROM employees").then(([data]) => {
+                for (let i = 0; i < data.length; i++) {
+                    updateEmployee[0].choices.push(data[i].name);
+                };
+
+                db.promise().query("SELECT title FROM roles").then(([data]) => {
+                    for (let i = 0; i < data.length; i++) {
+                        updateEmployee[1].choices.push(data[i].title);
+                    };
+                    
+                    inquirer
+                    .prompt(updateEmployee)
+                    .then((answers) => {
+                        const roles = new Roles(answers.listOfEmployeeRoles);
+                        roles.getSpecificRoleID().then(([data]) => {
+                            let employeesRoleId = JSON.stringify(data[0].id);
+
+                            let employeeNameArr = answers.listOfEmployees.split(' ');
+                            let firstEmpName = employeeNameArr[0];
+                            let lastEmpName = employeeNameArr[1];
+
+                            const employees = new Employees(firstEmpName, lastEmpName, employeesRoleId);
+                            employees.updateEmployeeRole().then(() => {
+                                init();
+                            });
+                        });
+                    });
+                });
+            }).catch((err) => {
+                console.log(err);
+            });
+        };
     });
 };
 
